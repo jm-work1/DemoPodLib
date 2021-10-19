35696de25a3409ffc902fe967b0e7e3ecbcf20ab
jianming-work <jm.work@qq.com>
2021-10-19 22:59:49 +0800

    test

---
+++ DemoPodLib/DemoPodLib.h
@@ -0,0 +1,18 @@
+//
+//  DemoPodLib.h
+//  DemoPodLib
+//
+//  Created by 黄坚明 on 2021/10/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoPodLib.
+FOUNDATION_EXPORT double DemoPodLibVersionNumber;
+
+//! Project version string for DemoPodLib.
+FOUNDATION_EXPORT const unsigned char DemoPodLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoPodLib/PublicHeader.h>
+
+
---
+++ DemoPodLib/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  DemoPodLib
+//
+//  Created by 黄坚明 on 2021/10/19.
+//
+
+import Foundation
+
+@objcMembers public class Test: NSObject
+{
+    public func TT()
+    {
+        print("Test TT")
+    }
+}
